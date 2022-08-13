fbf8f4b1ebf8c2a470e700e80bad1459a11b6e16
Justus Mehl <justus.mehl@gmx.de>
2022-08-13 18:52:01 +0200

    Added functionsfor checking solubility

--- 15-Puzzle-solver.lisp
+++ 15-Puzzle-solver.lisp
@@ -4,6 +4,8 @@
 
 (defparameter *initial-state* '(8 4 9 7 3 10 14 15 6 12 1 2 5 11 13 0))
 
+(defparameter *test-state* '(15 14 1 6 9 11 4 12 0 10 7 3 13 8 5 2))
+
 (defparameter *goal-state* '(1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 0))
 
 (defparameter *side-length* 4)
@@ -32,6 +34,30 @@
 (defparameter *queue* (make-array 1 :adjustable t :fill-pointer 0)
   "The priority-queue of the states to try next")
 
-(defun heuristic-1 (state)
-  )
 
+(defun member-sublist (item list)
+  (loop for sublist in list
+	if (member item sublist)
+	  return T
+	finally (return Nil)))
+
+(defun cycles (state)
+  (let ((cycles))
+    (loop for item in state
+	  do (when (not (member-sublist item cycles))
+	       (let ((current-item item) (cycle))
+		 (loop while (not (member current-item cycle))
+		       do (push current-item cycle)
+			  (setq current-item (nth (position current-item state) *goal-state*)))
+		 (push cycle cycles)
+		 )))
+    cycles))
+
+(defun number-transm (state)
+    (loop for cycle in (cycles state)
+	  if (> (length cycle) 1)
+	    sum (1- (length cycle)) into num-transm
+	  finally (return num-transm)))
+
+(defun solvable-p (state)
+  (evenp (+ (number-transm *goal-state*) (number-transm state))))
