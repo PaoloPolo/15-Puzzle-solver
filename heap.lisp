10ab8ee6a961f80838f1976708e62d10b232ce96
Justus Mehl <justus.mehl@gmx.de>
2022-10-17 13:43:51 +0200

    Heap implementation for a priority queue

--- heap.lisp
+++ heap.lisp
@@ -1,13 +1,13 @@
 ;;;; heap.lisp
 
-(in-package #:15-Puzzle-solver)
+;;;;(in-package #:15-Puzzle-solver)
 
 (defstruct (heap (:constructor %make-heap (compare-fn &key key)))
   "Structure for storing the type (min/max-heap) and heap itself"
   ;; Function for o
   (order-fn (if (null key)
 		compare-fn
-	      (lambda (x y)
+		(lambda (x y)
 		  (funcall compare-fn (funcall key x) (funcall key y)))))
   (heap-structure (make-array 0 :adjustable t :fill-pointer t)))
 
@@ -32,12 +32,12 @@
 
 (defun insert-heap (value heap)
   "Insert a given value into a heap"
-  (with-slots (order-fn heap-structure) heap
+  (with-slots (heap-structure) heap
     (percolate-up heap (vector-push-extend value heap-structure))))
 
 (defun remove-heap (heap)
   "Remove the topmost element of the heap and return it"
-  (with-slots (order-fn heap-structure) heap
+  (with-slots (heap-structure) heap
     (if (plusp (length heap-structure))
 	(let ((root (aref heap-structure 0)))
 	  (setf (aref heap-structure 0) (vector-pop heap-structure))
@@ -55,18 +55,21 @@
   (with-slots (heap-structure) heap
     (setf (fill-pointer heap-structure) 0)))
 
+(declaim (inline order-posn))
+(defun order-posn (posn-x posn-y heap)
+  (with-slots (heap-structure order-fn) heap
+    (funcall order-fn
+	     (aref heap-structure posn-x)
+	     (aref heap-structure posn-y))))
+
 (defun percolate-up (heap posn)
-  (with-slots (order-fn heap-structure) heap
+  (with-slots (heap-structure) heap
     (cond
-      ((and (/= posn 0)
-	    (not (funcall order-fn
-			  (aref heap-structure (get-parent posn))
-			  (aref heap-structure posn)
-			  )))
+      ((and (not (= posn 0))
+	    (not (order-posn (get-parent posn) posn heap)))
        (rotatef (aref heap-structure posn)
 		(aref heap-structure (get-parent posn)))
-       (percolate-up heap (get-parent posn))
-       )
+       (percolate-up heap (get-parent posn)))
       (t
        posn))))
 
@@ -74,23 +77,23 @@
 (defun percolate-down (heap posn)
   (with-slots (order-fn heap-structure) heap
     (multiple-value-bind (left-child right-child) (get-children posn)
-      (when (and (< (get-children posn)
-		    (length heap-structure))
-		 (not
-		  (and (funcall order-fn
-				(aref heap-structure posn)
-				(aref heap-structure right-child)
-				)
-		       (or
-			(>= (1+ (get-children posn)) (length heap-structure))
-			(funcall order-fn
-				 (aref heap-structure posn)
-				 (aref heap-structure right-child))))))
-	(let ((chosen-child (if (funcall order-fn
-					 (aref heap-structure left-child)
-					 (aref heap-structure right-child))
-				left-child
-				right-child)))
-	  (rotatef (aref heap-structure posn)
-		   (aref heap-structure chosen-child))
-	  (percolate-down heap chosen-child))))))
+      (flet ((swap-if-better (posn-x posn-y)
+	       (when (order-posn posn-x posn-y heap)
+		   (progn
+		    (rotatef (aref heap-structure posn-x)
+			     (aref heap-structure posn-y))
+		    (percolate-down heap posn-x)))))
+	(let ((length-index (1- (length heap-structure))))
+	  (cond
+	    ; Right and left child are existing
+	    ((<= right-child length-index)
+	     (if (order-posn left-child right-child heap)
+		 (swap-if-better left-child posn)
+		 (swap-if-better right-child posn)))
+	    ; Only left child is filled
+	    ((<= left-child length-index)
+	     (swap-if-better left-child posn))))))))
+	  
+	  
+	  
+      
